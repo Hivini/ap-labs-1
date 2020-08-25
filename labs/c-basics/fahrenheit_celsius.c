45e440e704ea23fceee284ec41f86ffb253f787e
Jorge Vinicio Quintero Santos <a01634502@itesm.mx>
2020-08-25 17:51:39 -0600

    Remove unnecessary definitions

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
 double convertToCelsius(double fahrenheit) {
 	return (fahrenheit - 32) * 5 / 9;
 }
