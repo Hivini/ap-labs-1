0192dd94121b2ea6b7ee51b7462f7018e776c719
Jorge Vinicio Quintero Santos <a01634502@itesm.mx>
2020-09-08 00:14:18 -0600

    Solved str-lib
    Signed-off-by: Jorge Vinicio Quintero Santos <a01634502@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,54 @@
+/**
+ * Author: Hivini
+ * Name: Jorge Vinicio Quintero Santos
+ * 
+ * Notes:
+ * The program returns 0 when an error happens to avoid any problems with the Makefile testing.
+ * The string.h library is only to compare the strings, no need to reinvent all the wheels :)
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+#define FLAG_ADD "-add"
+#define FLAG_FIND "-find"
+
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Not enough arguments needed, please type -add or -find.\n");
+        return 0;
+    }
+    char *flag = argv[1]; 
+    if (!strcmp(flag, "-add")) {
+        if (argc != 4) {
+            printf("Not enough arguments to add, need two strings.\n");
+            return 0;
+        }
+        char *origin = argv[2];
+        char *addition = argv[3];
+        char *result = mystradd(origin, addition);
+        printf("Initial Length\t\t: %d\n", mystrlen(origin));
+        printf("New String\t\t: %s\n", result);
+        printf("New Length\t\t: %d\n", mystrlen(result));
+    } else if (!strcmp(flag, "-find")) {
+        if (argc != 4) {
+            printf("Not enough arguments to find, need two strings.\n");
+            return 0;
+        }
+        char *origin = argv[2];
+        char *substr = argv[3];
+        int position = mystrfind(origin, substr);
+        if (position == -1) {
+            printf("['%s'] string was not found.\n", substr); 
+        } else {
+            printf("['%s'] string was found at [%d] position.\n", substr, position); 
+        }
+    } else {
+        printf("No proper flag given, please specify operation by adding -add or -find at the start.\n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,68 @@
+/**
+ * Author: Hivini
+ * Name: Jorge Vinicio Quintero Santos
+ */
+#include <stdio.h>
+#include <stdlib.h>
 
+#define END_STRING '\0'
 
 int mystrlen(char *str){
-    return 0;
+    char *c = NULL;
+    int length = 0;
+
+    // Iterate through each character in the pointer.
+    for (c = str; *c != END_STRING; c++) {
+        length++;
+    }
+    return length;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lorigin, laddition, size, i;
+    char *result = NULL;
+    lorigin = mystrlen(origin);
+    laddition = mystrlen(addition);
+    size = lorigin + laddition;
+    // One character is added for the end of string.
+    result = (char *) malloc(size + 1);
+
+    // Copy both of the strings to the result array.
+    for (i = 0; i < size; i++) {
+        if (i < lorigin) {
+            result[i] = origin[i];
+        } else {
+            result[i] = addition[i - lorigin];
+        }
+    }
+    result[i] = END_STRING;
+    return result;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int lorigin, lsubstr, i, psub, isub;
+    char *c = NULL;
+    lorigin = mystrlen(origin);
+    lsubstr = mystrlen(substr);
+    if (lorigin < lsubstr) {
+        return 0;
+    }
+
+    // psub - Position on the substring.
+    // isub - Position where the substring started appearing.
+    psub = isub = 0;
+    for (i = 0; i < lorigin; i++) {
+        if (origin[i] == substr[psub]) {
+            if (psub == 0) {
+                isub = i;
+            }
+            psub++;
+        } else {
+            psub = 0;
+        }
+        if (psub == lsubstr) {
+            return isub;
+        }
+    }
+    return -1;
 }
