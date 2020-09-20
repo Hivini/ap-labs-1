a46884493fcb6c07028ed9ddac8362e8a2b9a62c
Jorge Vinicio Quintero Santos <a01634502@itesm.mx>
2020-09-20 02:46:00 -0600

    Solved go-slices-maps
    Signed-off-by: Jorge Vinicio Quintero Santos <a01634502@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+// WordCount returns a map with the times a word appears in the string.
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	wordMap := make(map[string]int)
+	for w := range words {
+		word := words[w]
+		elem, present := wordMap[word]
+		if present {
+			wordMap[word] = elem + 1
+		} else {
+			wordMap[word] = 1
+		}
+	}
+	return wordMap
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -2,8 +2,16 @@ package main
 
 import "golang.org/x/tour/pic"
 
+// Pic function from Golang tour.
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	img := make([][]uint8, dy)
+	for x := range img {
+		img[x] = make([]uint8, dx)
+		for y := 0; y < dx; y++ {
+			img[x][y] = uint8(x*x + y*y)
+		}
+	}
+	return img
 }
 
 func main() {
